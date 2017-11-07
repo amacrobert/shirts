ccb86da04f9bbfd54ee75fe52358251a832403f3
amacrobert-meq <andrew.macrobert@gmail.com>
2017-11-06 23:58:36 -0500

    product filtering

---
+++ web/js/main.js
@@ -0,0 +1,27 @@
+$(function() {
+    $('.filter button').click(function() {
+
+        if ($(this).hasClass('active')) {
+            return;
+        }
+        var filter = $(this).data('filter');
+        $('.filter button:not(.' + filter + ')').attr('disabled', true);
+
+        var fadeTime = 350;
+        $('.filter button').removeClass('active');
+        $(this).addClass('active')
+
+        $('.product').animate({opacity: 0}, fadeTime, function() {
+            if (filter == 'all') {
+                $('.product').show();
+            }
+            else {
+                $('.product:not(.' + filter + ')').hide();
+                $('.product.' + filter).show();
+            }
+            $('.product').animate({opacity: 1}, fadeTime, function() {
+                $('.filter button').attr('disabled', false)
+            });
+        });
+    });
+});
